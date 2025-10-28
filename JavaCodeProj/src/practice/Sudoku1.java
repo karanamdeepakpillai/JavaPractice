79d12599712d472bbe0366f97e697db150cb0d0e
Deepak <Deepak@Deepak_PC>
2025-10-29 02:10:31 +0530

    updated loggers

--- JavaCodeProj/src/practice/Sudoku1.java
+++ JavaCodeProj/src/practice/Sudoku1.java
@@ -5,25 +5,21 @@ import java.util.*;
 public class Sudoku1 {
 
 	public static void main(String[] args) {
-		int a=5;
-		int b=3;
-		int c=a/b;
-		//System.out.println(2/3);
 		/*
 		 * Integer[][] matrix= {{0,8,0,0,1,0,7,0,6}, {7,0,0,6,9,0,8,3,0},
 		 * {0,6,4,0,0,0,2,0,0}, {2,9,0,0,8,7,0,0,3}, {0,5,0,2,0,3,0,6,0},
 		 * {4,0,0,9,6,0,0,2,8}, {0,0,8,0,0,0,3,7,0}, {0,3,7,0,2,8,0,0,4},
 		 * {6,0,5,0,3,0,0,8,0}};
 		 */
-		Integer[][] matrix={{5, 3, 0, 0, 7, 0, 0, 0, 0},
-	            {6, 0, 0, 1, 9, 5, 0, 0, 0},
-	            {0, 9, 8, 0, 0, 0, 0, 6, 0},
-	            {8, 0, 0, 0, 6, 0, 0, 0, 3},
-	            {4, 0, 0, 8, 0, 3, 0, 0, 1},
-	            {7, 0, 0, 0, 2, 0, 0, 0, 6},
-	            {0, 6, 0, 0, 0, 0, 2, 8, 0},
-	            {0, 0, 0, 4, 1, 9, 0, 0, 5},
-	            {0, 0, 0, 0, 8, 0, 0, 7, 9}};
+		Integer[][] matrix={{2, 0, 5, 0, 0, 9, 0, 0, 4},
+				            {0, 0, 0, 0, 0, 0, 3, 0, 7},
+				            {7, 0, 0, 8, 5, 6, 0, 1, 0},
+				            {4, 5, 0, 7, 0, 0, 0, 0, 0},
+				            {0, 0, 9, 0, 0, 0, 1, 0, 0},
+				            {0, 0, 0, 0, 0, 2, 0, 8, 5},
+				            {0, 2, 0, 4, 1, 8, 0, 0, 6},
+				            {6, 0, 8, 0, 0, 0, 0, 0, 0},
+				            {1, 0, 0, 2, 0, 0, 7, 0, 8}};
 		int zeroCount=0;
 		do{
 		zeroCount=0;
@@ -31,15 +27,23 @@ public class Sudoku1 {
 			for(int j=0;j<9;j++) {
 				//if(matrix[i][j]==0)
 					//getNumber(matrix, i, j);
-					System.out.print((matrix[i][j]=getNumber(matrix, i, j))+" ");
+					int val = matrix[i][j]=getNumber(matrix, i, j);
+					//System.out.print(val+" ");
 					if(matrix[i][j]==0)
 						zeroCount++;
 					
 			}
+			//System.out.println();
+		}
+		//System.out.println(zeroCount);
+		}while(zeroCount>0);
+		System.out.println("\nOutput:");
+		for(int i=0;i<9;i++) {
+			for(int j=0;j<9;j++) {
+				System.out.print(matrix[i][j]+" ");
+			}
 			System.out.println();
 		}
-		System.out.println(zeroCount);
-		}while(zeroCount>0);
 	}
 	
 	public static int getNumber(Integer[][] matrix,int x, int y) {
@@ -60,6 +64,7 @@ public class Sudoku1 {
 			}
 		}
 		if(numSet.size()==1) {
+			System.out.println("x:"+x+" y:"+y+" val:"+(int)(numSet.toArray())[0]);
 			return (int)(numSet.toArray())[0];
 		}
 		
